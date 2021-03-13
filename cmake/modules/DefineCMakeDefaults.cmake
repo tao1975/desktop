7d89aef0766eecfbf775e4557768cf58ee90dfa3
Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>
2021-03-13 14:25:43 -0500

    Fix and comment out default CMAKE_BUILD_TYPE
    Signed-off-by: Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>

--- cmake/modules/DefineCMakeDefaults.cmake
+++ cmake/modules/DefineCMakeDefaults.cmake
@@ -26,9 +26,9 @@ set(GENERIC_LIB_SOVERSION "0")
 set(CMAKE_ENABLE_WERROR ON)
 
 # Set the default build type to release with debug info
-if (NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE RelWithDebInfo
-    CACHE STRING
-      "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
-  )
-endif (NOT CMAKE_BUILD_TYPE)
+# if (NOT CMAKE_BUILD_TYPE)
+#     set(CMAKE_BUILD_TYPE RelWithDebInfo
+#         CACHE STRING
+#         "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
+#         FORCE)
+# endif (NOT CMAKE_BUILD_TYPE)
